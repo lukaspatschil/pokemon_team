10456ee928d2155a6e76450e5bc4b96802374d0e
lukas spatschil <51056202+lukaspatschil@users.noreply.github.com>
2021-03-01 20:04:31 +0100

    added filter

--- frontend/src/components/Selection.tsx
+++ frontend/src/components/Selection.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { gql, useMutation } from '@apollo/client';
 
 import Pokemon from './Pokemon';
@@ -17,7 +17,12 @@ const ADD_POKEMON = gql`
 
 const Selection = () => {
   const pokemons = useFetch("https://pokeapi.co/api/v2/pokemon?limit=151");
-  const [addPokemon, { data }] = useMutation(ADD_POKEMON);
+  const [addPokemon] = useMutation(ADD_POKEMON);
+
+  const [filtered, setFiltered] = useState(pokemons);
+  const [input, setInput] = useState('');
+
+  useEffect(() => setFiltered(pokemons), [pokemons]);
 
   const addTeam = (name: string, id: number) => {
     addPokemon({
@@ -29,12 +34,18 @@ const Selection = () => {
     window.location.reload()
   }
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+    setFiltered({ data: pokemons.data.filter(el => el.name.includes(e.target.value)), loading: false });
+  }
+
   return (
     <section className="App container">
       <h2>Select your pokemon:</h2>
+      <input type="text" onChange={handleChange} value={input} name="" id="" />
       <div className="flex">
         {
-          !pokemons.loading && pokemons?.data?.map((pokemon, id) => <Pokemon key={id} name={pokemon.name} id={pokemon.id} onClick={addTeam} picture={pokemon.sprites.front_default} />)
+          !pokemons.loading && filtered?.data?.map((pokemon, id) => <Pokemon key={id} name={pokemon.name} id={pokemon.id} onClick={addTeam} picture={pokemon.sprites.front_default} />)
         }
       </div>
     </section>
