0046b4e95fabca81f0f42613dc1178664d03a913
lukas spatschil <51056202+lukaspatschil@users.noreply.github.com>
2021-03-11 19:39:04 +0100

    fixed remove querry

--- frontend/src/components/Team.tsx
+++ frontend/src/components/Team.tsx
@@ -1,5 +1,5 @@
 import { ApolloError, gql, useMutation } from '@apollo/client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Pokemon from './Pokemon';
 import { PokemonGraph } from '../interfaces/pokemon.interface';
@@ -20,21 +20,21 @@ mutation RemovePokemon($id:Int!) {
 `;
 
 const Team = ({ loading, error, data }: Props) => {
-  const [removePokemon, something] = useMutation(REMOVE_POKEMON);
+  const [pokemons, setPokemons] = useState<PokemonGraph[]>(data ?? []);
+  const [removePokemon] = useMutation(REMOVE_POKEMON);
 
   const removeTeam = (name: string, id: number) => {
-    console.log(id);
     removePokemon({
       variables: {
-        id,
+        id: Number(id),
       }
     });
-    console.log(data?.filter(pokemon => pokemon.id !== id));
+    setPokemons(old => old.filter(pokemon => pokemon.id !== id));
   };
 
   useEffect(() => {
-    console.log(something);
-  }, [something])
+    setPokemons(data ?? []);
+  }, [data]);
 
   if (loading) return <section className="container mx-auto mt-10 mb-5 md:mb-10">Loading...</section>;
   if (error) return <section className="container mx-auto mt-10 mb-5 md:mb-10">Error :(</section>;
@@ -44,7 +44,7 @@ const Team = ({ loading, error, data }: Props) => {
       <h2 className="text-3xl font-bold">Your Team:</h2>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-4 xl:grid-cols-6">
         {
-          Array.isArray(data) && data.map((pokemon: PokemonGraph) => <Pokemon key={pokemon.id} name={pokemon.name} id={pokemon.id} picture={pokemon.picture} onClick={removeTeam} />)
+          Array.isArray(pokemons) && pokemons.map((pokemon: PokemonGraph) => <Pokemon key={pokemon.id} name={pokemon.name} id={pokemon.id} picture={pokemon.picture} onClick={removeTeam} />)
         }
       </div>
     </section>
